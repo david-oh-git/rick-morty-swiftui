120ed0bc07c00597e9e033e792360e3717255702
dave oh <hirakoshinji722@gmail.com>
2024-07-20 16:32:18 +0100

    Initial Commit

---
+++ rick and morty swiftui/rick_and_morty_swiftuiApp.swift
@@ -0,0 +1,32 @@
+//
+//  rick_and_morty_swiftuiApp.swift
+//  rick and morty swiftui
+//
+//  Created by shinji on 7/20/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct rick_and_morty_swiftuiApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
